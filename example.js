a8f3106bf362d57eacdf7c90bdd2b0ba7e71fcbf
Daveloper87 <dai.williams@bbc.co.uk>
2015-05-06 10:30:38 +0100

    Created an example js file

---
+++ example.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Require the MswClient: require('msw-client');
+var MswClient = require('./index');
+
+// Create a new instance with your apikey and spot_id
+var msw = new MswClient({
+
+    apikey: process.env.MSW_KEY,
+    spot_id: 1449
+});
+
+// ** Making a Request ** //
+
+// Classic Request
+msw.request(function(err, response){
+
+    console.log('Classic request: ', response);
+
+});
+
+// With Promises
+msw.exec().then(function(data){
+
+
+    console.log('Promise request: ', data);
+
+}).catch(function(err){
+
+    console.log(err);
+
+});
