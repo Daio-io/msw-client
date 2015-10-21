7e2558a7777a27fc2791a898cd5acc72e03cec5a
daveloper87 <dai.williams@bbc.co.uk>
2015-10-21 20:07:01 +0100

    Example request

---
+++ example.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Require the MswClient: require('msw-client');
+var MswClient = require('./index');
+
+// Create a new instance with your apikey and spot_id
+var msw = new MswClient({
+
+  apikey: process.env.MSW_KEY,
+  spot_id: 1449
+
+});
+
+// You can chain setters and fields
+msw.setSpotId(1449).setUnits('uk');
+
+// ** Making a Request ** //
+
+// Classic Request
+msw.request(function(err, response){
+
+  console.log('Classic request: ', response);
+
+});
+
+// With Promises no callback provided returns promise
+msw.request().then(function(data){
+
+  console.log('Promise request: ', data);
+
+}).catch(function(err){
+
+  console.log(err);
+
+});
